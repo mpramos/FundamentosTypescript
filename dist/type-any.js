957f88c4c2e07595833cb75bf11b0e4e1d3c6740
mpramos <mpramos78915400@gmail.com>
2022-02-25 09:43:22 -0400

    Usando el ANY

---
+++ dist/type-any.js
@@ -0,0 +1,21 @@
+"use strict";
+/* ANY
+    Sole se debe usar cuando no se se sepa cual es el valor
+    que se alamacenará en mi variable
+*/
+// Tipo expicito
+let IdUser;
+IdUser = 1; // number
+IdUser = '1'; // string
+console.log('idUser', IdUser);
+// Tipo Inferido
+let otherId;
+otherId = 1;
+otherId = '1';
+// otherId = true;
+console.log('otherId', otherId);
+// any se acerca a var 
+let surprise = 'hello typescript';
+//
+let re = surprise.substring(6);
+console.log('re:', re);
---
+++ src/type-any.ts
@@ -0,0 +1,23 @@
+/* ANY 
+    Sole se debe usar cuando no se se sepa cual es el valor
+    que se alamacenará en mi variable
+*/
+
+// Tipo expicito
+let IdUser : any;
+IdUser =1 ;// number
+IdUser ='1';// string
+console.log('idUser', IdUser)
+
+// Tipo Inferido
+let otherId;
+otherId =1;
+otherId='1';
+// otherId = true;
+console.log('otherId', otherId);
+// any se acerca a var 
+
+let surprise: any = 'hello typescript'
+//
+let re=surprise.substring(6)
+console.log('re:', re)
