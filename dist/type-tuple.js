47df8075ce8871b3baac7d76e8a3e12a932c124e
mpramos <mpramos78915400@gmail.com>
2022-02-25 21:18:05 -0400

    Tuplas de tuplas

--- dist/type-tuple.js
+++ dist/type-tuple.js
@@ -4,3 +4,21 @@ Object.defineProperty(exports, "__esModule", { value: true });
 let user;
 user = [1, 'luixaviles'];
 console.log('user', user);
+console.log('userName', user[1]);
+console.log('lenth', user[1].length);
+console.log('id', user[0]);
+// Tupas con varios valores
+// id , username, isPro
+let userInfo;
+userInfo = [1, 'maria', true];
+console.log('userInfo', userInfo);
+// Arreglo de Tupas
+let array = []; // [] el ultimo corchete es para decir que tendremos un conjutno de tuplas
+array.push([1, 'luixaviles']); // 0
+array.push([2, 'paparazzi']); // 1
+array.push([3, 'lensQueen']); // 2
+console.log('array1', array); //  3
+// Uso de funciones Array
+// lensQueen -> lensQueen001
+array[2][1] = array[2][1].concat('001');
+console.log('array2', array); //  3
--- src/type-tuple.ts
+++ src/type-tuple.ts
@@ -3,4 +3,24 @@ export {} ;
 // [1,'user']
 let user: [number, string];
 user = [1,'luixaviles']
-console.log('user', user)
+console.log('user', user);
+console.log('userName', user[1])
+console.log('lenth',user[1].length)
+console.log('id', user[0]) 
+// Tupas con varios valores
+// id , username, isPro
+let userInfo : [number, string , boolean]
+userInfo =[1,'maria', true];
+console.log('userInfo', userInfo);
+
+// Arreglo de Tupas
+let array: [number, string][] =[]; // [] el ultimo corchete es para decir que tendremos un conjutno de tuplas
+array.push([1,'luixaviles']); // 0
+array.push([2,'paparazzi']); // 1
+array.push([3,'lensQueen']); // 2
+console.log('array1',array) //  3
+
+// Uso de funciones Array
+// lensQueen -> lensQueen001
+array[2][1] = array[2][1].concat('001');
+console.log('array2',array) //  3
