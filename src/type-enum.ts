401857d09fea2f9496d54db0e5e13e69d4de51c1
mpramos <mpramos78915400@gmail.com>
2022-02-25 21:50:51 -0400

    ENUM

---
+++ src/type-enum.ts
@@ -0,0 +1,30 @@
+// Orientacion para Fotografias
+/* const landscape = 0;
+const portrait =1 ;
+const square =2;
+const panorama = 3; */
+enum PhotoOrientation {
+    Landscape =0,// 0 ocuparia la posicion 0
+    Portrait=1, // 1 ocuparia la posicion 1
+    Square=2, // 2 ocuparia la posicion 2
+    Panorama=3 // 3
+}
+const landscape: PhotoOrientation= PhotoOrientation.Landscape;
+console.log('landscape',landscape);
+console.log('Landscape',PhotoOrientation[landscape] )
+enum PictureOrientation {
+    Landscape =10,
+    Portrait, //11
+    Square, //12
+    Panorama //13
+}
+console.log('portrait', PictureOrientation.Portrait)
+enum Country {
+    Bolivia= 'bol',
+    Colombia = 'col',
+    Mexico = 'mex',
+    EEUU = 'usa',
+    España = 'esp'
+}
+const country: Country = Country.Colombia;
+console.log('country', country)
